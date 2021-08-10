19bb875198e530e9a85cc7c3dfdb7d29bdf600e3
HGC-teacher <85601103+HGC-teacher@users.noreply.github.com>
2021-08-11 09:18:21 +1000

    Added extra enter at EOF

--- hello.py
+++ hello.py
@@ -2,3 +2,4 @@ if __name__ == '__main__':
     my_name = 'Kylie'
     msg = f"{my_name} here, I'm struggling to make sense of everything that is going on here!"
     print('Hello everyone ' + msg + ':)')
+    
