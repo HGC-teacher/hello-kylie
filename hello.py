c9fae0d38947a3ab9c9a9ac7cf98f71ef790c9aa
HGC-teacher <85601103+HGC-teacher@users.noreply.github.com>
2021-08-11 08:37:00 +1000

    Adding personal message
    When I ran in VS code, my message it not print?

--- hello.py
+++ hello.py
@@ -1,2 +1,4 @@
 if __name__ == '__main__':
-    print('Hello everyone :)')
+    my_name = 'Kylie'
+    msg = f"{my_name} here, I'm struggling to make sense of everything that is going on here!"
+    print('Hello everyone ' + msg + ':)')
